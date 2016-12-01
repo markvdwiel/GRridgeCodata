159bd9aaa2b1cacd9dbe8819abf899181d72bd67
putriwikie <putriwikie@gmail.com>
2016-12-01 15:12:55 +0100

    Add files via upload

--- Cancer-consensus-genes/coDataProcessing_CancerConsensusGenes.R
+++ Cancer-consensus-genes/coDataProcessing_CancerConsensusGenes.R
@@ -1,32 +1,31 @@
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+#          1. Extract information of the cancer consensus genes               #
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+# source: http://cancer.sanger.ac.uk/census
+# The cancer consensus genes used in this study was stamped on November 25, 2015 
+# It contained of 572 cancer somatic genes. 
+# The file is also available at:
+# https://github.com/markvdwiel/GRridgeCodata/tree/master/Cancer-consensus-genes
+# "Census_allWed Nov 25 11_56_18 2015.csv"
 
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
-#                   Read the downloaded cancer consensus list                      #
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
-COSMIC = read.csv("Census_allWed Nov 25 11_56_18 2015.csv")
+datCosmic = read.csv("Census_allWed Nov 25 11_56_18 2015.csv")
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 
 
-
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
-#  Re-group the somatic consensus genes based on the type of cancers of interest   #
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
-# The listed genes were next used to group genes in the primary data set into three 
-# groups, namely (1) cancer specific genes, i.e. genes that are known to mutate in
-# cancer diseases of interest (2) cancer genes, i.e. known mutated genes in the 
-# other than the cancer specific genes; and (3) genes that were not stated as a
-# cancer mutated gene by the COSMIC list. 
-# This section provides a preprocessing step to re-group the cancer somatic genes
-# into group (1) and group (2).
-# Example: Lung and Pancreas cancer case
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+#               2. Re-group the somatic consensus genes                       #
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+# The grouping is based on the type of cancers of interest
 idLung = idPancreas = idBoth = c()
-# idLung/idPancreas/idBoth: a vector containing indeces of mutated genes in 
+# idLung/idPancreas/idBoth: a vector containing indices of mutated genes in 
 # lung cancer/pancreatic cancer/both cancers
-for(i in 1:dim(COSMIC)[1]){
-  temp = as.character(COSMIC[i,8])
+for(i in 1:dim(datCosmic)[1]){
+  temp = as.character(datCosmic[i,8])
   temp2 = unlist(strsplit(temp,", "))
   
-  # As one gene possibly cause more than one cancer,
-  # we check which mutated genes in the spesific cancers of interest,
-  # in this case, lung and pancreas cancer
+  # As one gene possibly causes more than one cancer,
+  # we check which mutated genes in the spesific cancers of interest 
+  #(i.e. NSCLC and pancreatic cancer)
   checkA1 = "lung" %in% temp2
   checkA2 = "NSCLC" %in% temp2
   checkB1 = "pancreas" %in% temp2
@@ -39,12 +38,29 @@ for(i in 1:dim(COSMIC)[1]){
   else{next}
 }
 
-# Re-group the somatic cancer genes into (1) NSCLCpancreatic and (2) other cancers
+# Re-group the somatic cancer types into (1) NSCLCpancreatic and (2) other cancer
 idAll = unique(c(idLung,idPancreas,idBoth))
-cosmic =  replicate(nrow(COSMIC),"other cancers")
+cosmic =  replicate(nrow(datCosmic),"other cancers")
 cosmic[idAll] = "NSCLCpancreatic"
-names(cosmic) = COSMIC[,1]
 table(cosmic)
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+#       3. Assign the somatic cancer genes information to each of feature     #
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+# "genesWurdinger" is an object (vector), 
+# containing the gene symbol from the mRNAseq data set
+# help(dataWurdinger) shows how to get the "genesWurdinger" object.
+is = intersect(datCosmic[,1], genesWurdinger)
+ig = match(is, genesWurdinger)
+ic = match(is,datCosmic[,1])
+cosmic2 = replicate(length(genesWurdinger),"noncosmic")
+cosmic2[ig] = cosmic[ic]
+cosmic2 = as.factor(cosmic2)
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
+
 
 # NOTE:
 # 
